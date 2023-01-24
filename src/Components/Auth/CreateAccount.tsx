480a5c6dade485cc9fef2b9550c3a511b0a88ca9
debbiesanni <career.debbie@gmail.com>
2023-01-24 00:02:31 +0000

    style-bugfix and create account design

--- src/Components/Auth/CreateAccount.tsx
+++ src/Components/Auth/CreateAccount.tsx
@@ -1,12 +1,103 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+import Image from "next/image";
+
 import useStyles from "./styles";
 
 const CreateAccount = () => {
   const authStyles = useStyles();
   return (
-    <Box className={authStyles.bg} style={{}}>
-      create-account
-      <input/>
+    <Box component="main">
+      <Grid container>
+        <Grid item xs={12} md={4} className={authStyles.imgContainer}>
+          <Box
+            ml={7}
+            component="aside"
+            className={`${authStyles.displayColumnWithHeight} ${authStyles.JustifySpaceBtw}`}
+          >
+            <Box>
+              <Typography variant="h2" component="h1" mt={6} color="primary">
+                safeSpace
+              </Typography>
+              <Typography
+                variant="h4"
+                component="article"
+                mt={4}
+                color="primary"
+              >
+                Welcome to your safe heaven. <br />
+                create your space <br />
+                where you can say it all <br />
+                with no one judging you
+              </Typography>
+            </Box>
+            <Box>
+              <Image
+                src="/images/create-account/create.svg"
+                alt=""
+                height={500}
+                width={550}
+              />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={8} sx={{ background: "#fff" }}>
+          <Container
+            maxWidth="sm"
+            className={`${authStyles.displayColumnWithHeight} ${authStyles.JustifyCenter}`}
+          >
+            <Stack spacing={4}>
+              <Typography
+                variant="h4"
+                component="text"
+                mb={3}
+                className={`${authStyles.textAlignCentre}`}
+              >
+                Create your safespace (Sign-up)
+              </Typography>
+              <TextField
+                id="username"
+                name="username"
+                label="Username"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="email"
+                name="email"
+                label="Email address"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                fullWidth
+              />
+              <TextField
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirm Password"
+                variant="outlined"
+                fullWidth
+              />
+              <Box>
+                <Button variant="contained" className={authStyles.btn}>
+                  Create account
+                </Button>
+              </Box>
+            </Stack>
+          </Container>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -5,16 +5,154 @@ import { red } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#FFF212",
+      main: "#191970",
     },
     secondary: {
-      main: "#FFF212",
+      main: "#F0FFFF",
     },
     error: {
       main: red.A400,
     },
     background: {
       default: "#fff",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    h1: {
+      fontWeight: 700,
+      color: "#333333",
+      lineHeight: 1.125,
+      fontSize: "clamp(2rem, -0.875rem + 8.333vw, 3.25rem)",
+    },
+    h2: {
+      fontWeight: 700,
+      color: "#333333",
+      lineHeight: 1.125,
+      fontSize: "clamp(1.875rem, -0.85rem + 8.333vw, 3rem)",
+    },
+    h3: {
+      fontWeight: 600,
+      color: "#333333",
+      lineHeight: 1.125,
+      fontSize: "clamp(1.75rem, -0.8rem + 7vw, 2.725rem)",
+    },
+    h4: {
+      // fontSize: "1.75rem",
+      fontWeight: 600,
+      color: "#333333",
+      fontSize: "clamp(1.5rem, -0.6rem + 6vw, 2rem)",
+    },
+    h5: {
+      fontSize: "1.25rem",
+      fontWeight: 500,
+      color: "#333333",
+    },
+    h6: {
+      fontSize: "1rem",
+      fontWeight: 500,
+      color: "#333333",
+    },
+    body1: {
+      color: "#616161",
+    },
+    body2: {
+      color: "#616161",
+    },
+    caption: {
+      color: "#616161",
+      lineHeight: "1rem",
+    },
+    fontFamily: ["'Matter'", "sans-serif"].join(","),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `@font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-Heavy.otf') format("truetype");
+          font-weight: 900;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-HeavyItalic.otf') format("truetype");
+          font-weight: 900;
+          font-style: italic;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-Bold.otf') format("truetype");
+          font-weight: 700;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-BoldItalic.otf') format("truetype");
+          font-weight: 700;
+          font-style: italic;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-SemiBold.otf') format("truetype");
+          font-weight: 600;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-SemiBoldItalic.otf') format("truetype");
+          font-weight: 600;
+          font-style: italic;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-Medium.otf') format("truetype");
+          font-weight: 500;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-MediumItalic.otf') format("truetype");
+          font-weight: 500;
+          font-style: italic;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-Regular.otf') format("truetype");
+          font-weight: 400;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-RegularItalic.otf') format("truetype");
+          font-weight: 400;
+          font-style: italic;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-Light.otf') format("truetype");
+          font-weight: 300;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Matter';
+          src: url('/fonts/matter/Matter-LightItalic.otf') format("truetype");
+          font-weight: 300;
+          font-style: italic;
+          font-display: swap;
+        }`,
     },
   },
 });
